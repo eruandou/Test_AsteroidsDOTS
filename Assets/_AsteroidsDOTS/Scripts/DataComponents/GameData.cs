f3dbf8bbf8111cf2753cd02f4bcd204c22a32daa
eruandou <zambra_nacho@hotmail.com>
2023-05-27 00:44:26 -0300

    Add instancing data to GameData

--- Assets/_AsteroidsDOTS/Scripts/DataComponents/GameData.cs
+++ Assets/_AsteroidsDOTS/Scripts/DataComponents/GameData.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 namespace _AsteroidsDOTS.Scripts.DataComponents
 {
-    public enum EnemyTypes
-    {
-        SmallUfo,
-        BigUfo
-    }
-
     [GenerateAuthoringComponent]
     public struct GameData : IComponentData
     {
         [Header("Enemies")] public float2 SpawnEnemyTime;
         public Entity SmallUfo;
         public Entity BigUfo;
+        public int EnumAmount;
     }
 }
---
+++ Assets/_AsteroidsDOTS/Scripts/Enums/EnemyTypes.cs
@@ -0,0 +1,8 @@
+namespace _AsteroidsDOTS.Scripts.Enums
+{
+    public enum EnemyTypes
+    {
+        SmallUfo = 0,
+        BigUfo = 1
+    }
+}
