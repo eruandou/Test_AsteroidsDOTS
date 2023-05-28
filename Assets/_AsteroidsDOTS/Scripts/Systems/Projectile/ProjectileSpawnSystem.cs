e5917d8098d8e6d7ce26b83b10b978abedd1b5a0
eruandou <zambra_nacho@hotmail.com>
2023-05-28 04:45:00 -0300

    Add shooting modifier to shoot system

--- Assets/_AsteroidsDOTS/Scripts/Systems/Projectile/ProjectileSpawnSystem.cs
+++ Assets/_AsteroidsDOTS/Scripts/Systems/Projectile/ProjectileSpawnSystem.cs
@@ -1,7 +1,9 @@
 using _AsteroidsDOTS.Scripts.DataComponents;
 using _AsteroidsDOTS.Scripts.DataComponents.Tags;
+using _AsteroidsDOTS.Scripts.DevelopmentUtilities;
 using _AsteroidsDOTS.Scripts.Globals;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 
 namespace _AsteroidsDOTS.Scripts.Systems.Projectile
@@ -24,24 +26,38 @@ namespace _AsteroidsDOTS.Scripts.Systems.Projectile
             }
 
             var l_ecb = m_endSimulationBuffer.CreateCommandBuffer();
-            Entities.WithAll<PlayerTag>().ForEach((ref ShootingData p_shootingData, in LocalToWorld p_localToWorld,
-                    in Rotation p_rotation) =>
+            Entities.ForEach(
+                (ref ShootingData p_shootingData, in LocalToWorld p_localToWorld, in Rotation p_rotation,
+                    in ShootingStatsPowerUpData p_shootingStats) =>
                 {
                     if (!p_shootingData.ShouldShootProjectile) return;
 
+                    //TODO: Add shooting modificator here
+
+                    for (int i = 0; i < p_shootingStats.BulletsToShoot; i++)
+                    {
+                        //Instantiate the projectile entity and set its initial position and 
+                        Entity l_projectileEntity = l_ecb.Instantiate(p_shootingData.ProjectilePrefab);
+                        var l_deviationVector =
+                            math.mul(p_rotation.Value,
+                                quaternion.AxisAngle(Float3Constants.Up, i * p_shootingStats.BulletAngleDifference));
+                        var l_intendedForwards = math.forward(l_deviationVector);
+                        UninitializedProjectileTag l_projectileTag = new UninitializedProjectileTag()
+                            { IntendedForwards = l_intendedForwards };
+                        l_ecb.AddComponent(l_projectileEntity, l_projectileTag);
+                        var l_spawnPosition = p_localToWorld.Position + p_localToWorld.Forward *
+                            p_shootingData.ProjectileSpawnForwardOffset;
+                        var l_projectileTranslation = new Translation() { Value = l_spawnPosition };
+                        l_ecb.SetComponent(l_projectileEntity, l_projectileTranslation);
+                        var l_rotation = new Rotation()
+                        {
+                            Value = l_deviationVector
+                        };
+                        // { Value = quaternion.LookRotation(l_intendedForwards, Float3Constants.Up) };
+                        l_ecb.SetComponent(l_projectileEntity, l_rotation);
+                    }
 
                     p_shootingData.ShouldShootProjectile = false;
-
-                    //Instantiate the projectile entity and set its initial position and 
-                    Entity l_projectileEntity = l_ecb.Instantiate(p_shootingData.ProjectilePrefab);
-                    UninitializedProjectileTag l_projectileTag = new UninitializedProjectileTag()
-                        { IntendedForwards = p_localToWorld.Forward };
-                    l_ecb.AddComponent(l_projectileEntity, l_projectileTag);
-                    var l_spawnPosition = p_localToWorld.Position + p_localToWorld.Forward *
-                        p_shootingData.ProjectileSpawnForwardOffset;
-                    var l_projectileTranslation = new Translation() { Value = l_spawnPosition };
-                    l_ecb.SetComponent(l_projectileEntity, l_projectileTranslation);
-                    l_ecb.SetComponent(l_projectileEntity, p_rotation);
                 }
             ).Schedule();
 
