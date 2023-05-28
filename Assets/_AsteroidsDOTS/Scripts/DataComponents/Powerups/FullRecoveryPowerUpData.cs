d59c6bde0e43e8c0d619b89b2c359ffaffcdaea4
eruandou <zambra_nacho@hotmail.com>
2023-05-28 04:20:19 -0300

    Powerup prefabs

---
+++ Assets/_AsteroidsDOTS/Scripts/DataComponents/Powerups/FullRecoveryPowerUpData.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+namespace _AsteroidsDOTS.Scripts.DataComponents.Powerups
+{
+    [GenerateAuthoringComponent]
+    public struct FullRecoveryPowerUpData : IPowerUp
+    {
+    }
+}
