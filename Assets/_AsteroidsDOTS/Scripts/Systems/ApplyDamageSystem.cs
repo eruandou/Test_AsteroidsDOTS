acc8b94fbc524a3622160231d9de5cf260dd7ba2
eruandou <zambra_nacho@hotmail.com>
2023-05-28 22:04:38 -0300

    Implement Invulnerability on damage applier system

--- Assets/_AsteroidsDOTS/Scripts/Systems/ApplyDamageSystem.cs
+++ Assets/_AsteroidsDOTS/Scripts/Systems/ApplyDamageSystem.cs
@@ -6,14 +6,29 @@ namespace _AsteroidsDOTS.Scripts.Systems
     [UpdateInGroup(typeof(LateSimulationSystemGroup))]
     public class ApplyDamageSystem : SystemBase
     {
+        protected override void OnCreate()
+        {
+            RequireForUpdate(GetEntityQuery(ComponentType.ReadWrite<EntityHealthData>()));
+        }
+
         protected override void OnUpdate()
         {
+            var l_deltaTime = Time.DeltaTime;
             Entities.ForEach((ref EntityHealthData p_healthData) =>
             {
+                p_healthData.CurrentInvincibilityTime -= l_deltaTime;
+
                 if (p_healthData.PendingHealthModification == 0) return;
+
+                if (p_healthData.IsInvincible)
+                {
+                    p_healthData.PendingHealthModification = 0;
+                    return;
+                }
 
                 p_healthData.Health += p_healthData.PendingHealthModification;
                 p_healthData.PendingHealthModification = 0;
+                p_healthData.CurrentInvincibilityTime = p_healthData.InvincibilityTime;
             }).ScheduleParallel();
         }
     }
